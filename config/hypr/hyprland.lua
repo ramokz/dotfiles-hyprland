3c7f0a2afbb82696b293a7a95347fc58ebea459e
ramokz <Hello@MarcusSkov.com>
2026-06-27 11:26:45 +0100

    Updated dotfiles

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -18,7 +18,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 
-	hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size 20")
+	hl.exec_cmd("hyprctl setcursor MyCursor 14")
 end)
 
 -- █▀▀ █▄░█ █░█
