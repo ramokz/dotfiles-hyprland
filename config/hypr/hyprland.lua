0851bdee883dcc8ff912e8b3a9500c24fdfcc5f4
ramokz <Hello@MarcusSkov.com>
2026-06-27 11:27:28 +0100

    Updated dotfiles

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -18,7 +18,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 
-	hl.exec_cmd("hyprctl setcursor MyCursor 14")
+	hl.exec_cmd("hyprctl setcursor MyCursor 24")
 end)
 
 -- █▀▀ █▄░█ █░█
